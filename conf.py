c9fa4875b62710ced46a9d64e1211970ea8dbcfd
Mrt134 <timmoore88@gmail.com>
2017-01-17 15:08:32 -0500

    Update handbook style: - Move lumina handbook to the more up to date trueos style. - Added wispy as a transparent background to the TOC. - fixed a background color error in the TOC. - tentative addition of advertising and search functionality.

--- conf.py
+++ conf.py
@@ -144,7 +144,7 @@ roles.register_local_role("menuselection", patched_menusel_role)
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'lumina_style'
+html_theme = 'trueos_style'
 html_theme_path = ["themes",]
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -174,7 +174,7 @@ html_favicon = "lumina.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
